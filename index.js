4f92c259dbb18a60cf368d86e647962bb311b5b2
ralphZero <placideralph@gmail.com>
2022-07-26 21:45:31 -0400

    update car enpoint created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // import express
 import express from 'express';
 
+// import route functions
+import { getCars, addCar, updateCar } from './src/cars.js';
 // create a new server instance
 const app = express();
 
@@ -10,7 +12,15 @@ const PORT = 3333;
 // will recieve json data
 app.use(express.json());
 
-// define the routes
+// -- define the routes --
+// get all cars from db
+app.get('/cars', getCars);
+
+// create new car
+app.post('/cars', addCar);
+
+// update an existing car
+app.patch('/cars/:id', updateCar);
 
 // listen to the server on the specified port
 app.listen(PORT, () => {
---
+++ src/cars.js
@@ -0,0 +1,61 @@
+// import mongoClient
+import { client } from './dbconnect.js';
+import { ObjectId } from 'mongodb';
+
+
+
+// get all cars: GET
+export const getCars = (req, res) => {
+    client.connect((err) => {
+        if(err) {
+            res.status(500).send(err);
+            return;
+        }
+        const collection = client.db('test').collection('cars');
+        collection.find().toArray((err, result) => {
+            if(err) res.status(500).send(err);
+            if(result) res.json(result);
+            client.close();
+        });
+    });
+};
+
+// add new car : POST
+export const addCar = (req, res) => {
+    client.connect((err) => {
+        if(err) {
+            res.status(500).send(err);
+            return;
+        }
+        const car = req.body;
+        const collection = client.db('test').collection('cars');
+        collection.insertOne(car, (err, result) => {
+            if(err) res.status(500).send(err);
+            if(result) res.json(result);
+            client.close();
+        });
+    });
+    
+};
+
+// update car with id
+export const updateCar = (req, res) => {
+    client.connect((err) => {
+        if(err) {
+            res.status(500).send(err);
+            return;
+        }
+        const carId = req.params.id;
+        const car = req.body;
+        const collection = client.db('test').collection('cars');
+        collection.updateOne(
+            { _id: ObjectId(carId) },
+            {  $set: car },
+            (err, result) => {
+                if(err) res.status(500).send(err);
+                if(result) res.json(result);
+                client.close();
+            }
+        )
+    })
+};
